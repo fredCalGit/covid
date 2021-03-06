482fb86ae87b840690dd734c91d2ac69c8bb2bb6
Fred Cal <fred.cal@hotmail.com>
2021-03-06 03:50:31 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './App.module.css'
+import {Cards, Chart, CountryPicker} from './components'
+import {fetchData} from './api'
+import logo from './images/image.png'
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country: ''
+    }
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country)
+        this.setState({data: fetchedData, country: country})
+    }
+    async componentDidMount() {
+        const fetchedData = await fetchData()
+        this.setState({data: fetchedData})
+    }
+    render() {
+        const {data, country} = this.state
+        return (
+            <div className={styles.container}>
+                <img src={logo} className={styles.image} alt="COVID-19"/>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+            </div>
+        )
+    }
+}
+
+export default App
